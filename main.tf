6151ad0c81d2feeb65e0c856e638951a7aecc687
Ivan <73355620+doskochynskyi@users.noreply.github.com>
2022-02-02 00:56:01 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,16 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  use_msi = true
+
+  backend "azurerm" {
+    #storage_account_name = "abcd1234"
+    #container_name       = "tfstate"
+    #key                  = "prod.terraform.tfstate"
+    subscription_id      = "91c428c6-87f0-42a0-970a-bfc0d94cb6af"
+    tenant_id            = "b04ac3d3-fff5-4444-8d9e-b3b7d67cf78e"
+  }
 }
 
 resource "azurerm_resource_group" "rg" {
