e7c452727531cad7df4ebaa58a4d96bf98ae25c2
Ivan <73355620+doskochynskyi@users.noreply.github.com>
2022-02-06 15:00:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,25 +8,27 @@ terraform {
     }
   }
   
-    backend "azurerm" {
-    resource_group_name  = "RG-Jenkins"
-    storage_account_name = "saterraformst"
-    container_name       = "conttfstate"
-    key                  = "terraform.tfstate"
-    use_msi              = true
-    subscription_id      = "91c428c6-87f0-42a0-970a-bfc0d94cb6af"
-    tenant_id            = "b04ac3d3-fff5-4444-8d9e-b3b7d67cf78e"
-  }
+ #   backend "azurerm" {
+ #   resource_group_name  = "RG-Jenkins"
+ #   storage_account_name = "saterraformst"
+ #   container_name       = "conttfstate"
+ #   key                  = "terraform.tfstate"
+ #   use_msi              = true
+ #   subscription_id      = "91c428c6-87f0-42a0-970a-bfc0d94cb6af"
+ #   tenant_id            = "b04ac3d3-fff5-4444-8d9e-b3b7d67cf78e"
+ # }
 }
 
 # Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
-
+  
+    subscription_id      = "91c428c6-87f0-42a0-970a-bfc0d94cb6af"
+    tenant_id            = "b04ac3d3-fff5-4444-8d9e-b3b7d67cf78e"
 }
 
 
 resource "azurerm_resource_group" "rg" {
-  name     = "myTFResourceGroup"
-  location = "westus2"
+  name     = "RGmanagedIdentity"
+  location = "eastus"
 }
